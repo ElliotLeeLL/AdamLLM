7a56224d38db25c0cacc024f2c3538162ce63401
ElliotLeeLL <xingyuli1996@gmail.com>
2025-05-20 19:48:07 +0800

    Reorganize the utils module

---
+++ utils/model_utils.py
@@ -0,0 +1,78 @@
+import torch
+import tiktoken
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allow_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probs = torch.softmax(logits, dim=-1)
+        idx_next = torch.maxarg(probs, dim=-1, keep_dim=True)
+        idx = torch.cat([idx, idx_next], dim=1)
+    return idx
+
+def evaluate_model(
+    model, train_loader, val_loader, device, eval_iter
+):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(start_dim=0, end_dim=1), target_batch.flatten()
+    )
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch=input_batch, target_batch=target_batch, model=model, device=device
+            )
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+
+def generate_and_print_sample(
+    model, tokenizer, device, start_context
+):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded, max_new_tokens=50, context_size=context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace('\n', ' '))
+    model.train()
