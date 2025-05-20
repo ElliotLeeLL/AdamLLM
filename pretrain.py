a4b4fdf539d144a0129e3432369ef5c54c1153d4
ElliotLeeLL <xingyuli1996@gmail.com>
2025-05-20 19:48:17 +0800

    Update pretrain.py

---
+++ pretrain.py
@@ -0,0 +1,71 @@
+import tiktoken
+import torch
+from configuration import config
+from model import AdamLLMModel
+from utils.model_utils import calc_loss_batch, evaluate_model, generate_and_print_sample
+from dataset.adam_dataset import create_dataloader
+
+def train_model_simple(
+        model,
+        train_loader,
+        val_loader,
+        optimizer,
+        device,
+        num_epochs,
+        eval_freq,
+        eval_iter,
+        start_context,
+        tokenizer
+):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(
+                    f"Ep {epoch + 1} (Step {global_step:06D})"
+                    f"Train loss {train_loss:.3f}"
+                    f"Val loss {val_loss:.3f}"
+                )
+
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+
+    return train_losses, val_losses, track_tokens_seen
+
+# Prepare the dataset
+# TODO
+
+torch.manual_seed(42)
+tokenizer = tiktoken.get_encoding("gpt2")
+model = AdamLLMModel(config)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=0.0004,
+    weight_decay=0.1
+)
+num_epochs = 10
+train_losses, val_losses, track_tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs,
+    eval_freq=5, eval_iter=5, start_context="Life is beautiful", tokenizer=tokenizer
+
+)
